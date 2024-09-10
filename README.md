f6c4a4ac190982a060981141dc7066c5f78f97f5
Erin2021 <appleyummy78@gmail.com>
2024-09-11 00:21:41 +0900

    11. Success and Error Callbacks

--- README.md
+++ README.md
@@ -12,3 +12,4 @@
 8. Refetch Defaults
 9. Polling
 10. useQuery on click
+11. Success and Error Callbacks
--- src/components/RQSuperHerosPage.tsx
+++ src/components/RQSuperHerosPage.tsx
@@ -1,12 +1,25 @@
 import { useQuery } from "react-query";
 import axios from "axios";
 import { Tdata } from "./SuperHeroesPage";
+import { useState } from "react";
 
 const fetchSuperHeroes = () => {
   return axios.get("http://localhost:4000/superheroes");
 };
 
 const RQSuperHeroesPage = () => {
+  const[duration,setDuration]=useState<any>(3000);
+
+  const onSuccess=(data: any)=>{
+    if(data?.data.length === 4)setDuration(false)
+    console.log('Perform side effect after data fetching',data)
+  }
+
+  const onError=(error: any)=>{
+    if(error)setDuration(false);
+    console.log('Perform side effect after encountering error',error)
+  }
+
   //구조분할로 필요한것만 가져옴(https://tanstack.com/query/v4/docs/framework/react/reference/useQuery)
   const { isLoading, data, isError, error, isFetching,refetch } = useQuery(
     "super-heroes",
@@ -18,9 +31,11 @@ const RQSuperHeroesPage = () => {
       //refetchOnWindowFocus:true,
       //refetchInterval:2000,
       //refetchIntervalInBackground:true,
-      enabled:false,//fetching disabled.refetchOnMount disabled 한거임
+      //enabled:false,//fetching disabled.refetchOnMount disabled 한거임
 
-
+      onSuccess,
+      onError,
+      refetchInterval:duration,
     }
   );
 
@@ -46,4 +61,4 @@ const RQSuperHeroesPage = () => {
 export default RQSuperHeroesPage;
 
 //data?.data : data값이 useQuery로 불려왔는데, 데이터를 사용하려면 data에 들어가야 사용할 수 있음.
-//에러판정까지 시간이 걸리는이유:에러나면 자동으로 재도전을 하거든: error as Error : 에러가 에러타입일때 작동하는거
+//에러판정까지 시간이 걸리는이유:에러나면 자동으로 3번 재도전을 하거든: error as Error : 에러가 에러타입일때 작동하는거
